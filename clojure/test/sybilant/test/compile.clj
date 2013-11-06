a732f6a378effa7f76f7544f76a93be4af4258bb
Paul Stadig <paul@sybilant.org>
2013-11-05 19:19:23 -0500

    Generates initial project structure.

---
+++ clojure/test/sybilant/test/compile.clj
@@ -0,0 +1,15 @@
+;;;; Copyright © 2013 Paul Stadig. All rights reserved.
+;;;;
+;;;; This Source Code Form is subject to the terms of the Mozilla Public
+;;;; License, v. 2.0. If a copy of the MPL was not distributed with this file,
+;;;; You can obtain one at http://mozilla.org/MPL/2.0/.
+;;;;
+;;;; This Source Code Form is "Incompatible With Secondary Licenses", as defined
+;;;; by the Mozilla Public License, v. 2.0.
+(ns sybilant.test.compile
+  (:require [clojure.test :refer :all]
+            [sybilant.compile :refer :all]))
+
+(deftest test-main
+  (is (= "Hello, Sybilant!\n" (with-out-str (-main))))
+  (is (= "Hello, Sybilant! foo\n" (with-out-str (-main "foo")))))
