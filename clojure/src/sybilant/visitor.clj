27aa943a051b13c43001c1aaa2248ce3bd56e3d9
Paul Stadig <paul@sybilant.org>
2013-11-06 06:59:01 -0500

    Implement visitor.
    Visits symbols, numbers, registers, instructions, defasm, and defextern.

---
+++ clojure/src/sybilant/visitor.clj
@@ -0,0 +1,41 @@
+;;;; Copyright © 2013 Paul Stadig. All rights reserved.
+;;;;
+;;;; This Source Code Form is subject to the terms of the Mozilla Public
+;;;; License, v. 2.0. If a copy of the MPL was not distributed with this file,
+;;;; You can obtain one at http://mozilla.org/MPL/2.0/.
+;;;;
+;;;; This Source Code Form is "Incompatible With Secondary Licenses", as defined
+;;;; by the Mozilla Public License, v. 2.0.
+(ns sybilant.visitor
+  (:require [clojure.zip :as z]
+            [sybilant.parser :refer [error]]))
+
+(defn branch? [exp]
+  (contains? #{:instruction :defasm :defextern} (:type exp)))
+
+(defmulti children (comp :type first list))
+(defmethod children :instruction [{:keys [operator operands]}]
+  (cons operator operands))
+(defmethod children :defasm [{:keys [name statements]}]
+  (cons name statements))
+(defmethod children :defextern [{:keys [name]}]
+  [name])
+(defmethod children :default [exp]
+  (error "not a branch node" exp))
+
+(defmulti make-node (comp :type first list))
+(defmethod make-node :instruction [exp [operator & operands]]
+  (assoc exp :operator operator :operands operands))
+(defmethod make-node :defasm [exp [name & statements]]
+  (assoc exp :name name :statements statements))
+(defmethod make-node :defextern [exp [name]]
+  (assoc exp :name name))
+(defmethod make-node :default [exp children]
+  (error "not a branch node" exp))
+
+(defn visit [exp visitor]
+  (let [zip (z/zipper branch? children make-node exp)]
+    (loop [loc zip]
+      (if-not (z/end? loc)
+        (recur (z/next (z/edit loc visitor)))
+        (z/root loc)))))
---
+++ clojure/test/sybilant/test/visitor.clj
@@ -0,0 +1,35 @@
+;;;; Copyright © 2013 Paul Stadig. All rights reserved.
+;;;;
+;;;; This Source Code Form is subject to the terms of the Mozilla Public
+;;;; License, v. 2.0. If a copy of the MPL was not distributed with this file,
+;;;; You can obtain one at http://mozilla.org/MPL/2.0/.
+;;;;
+;;;; This Source Code Form is "Incompatible With Secondary Licenses", as defined
+;;;; by the Mozilla Public License, v. 2.0.
+(ns sybilant.test.visitor
+  (:refer-clojure :exclude [number? symbol?])
+  (:require [clojure.test :refer :all]
+            [sybilant.parser :refer :all]
+            [sybilant.visitor :refer :all]))
+
+(defn visitor [n]
+  (assoc n :visited? true))
+
+(deftest test-visit-instruction
+  (let [visited (visit (parse-instruction '(%add %rax 1)) visitor)]
+    (is (get-in visited [:visited?]))
+    (is (get-in visited [:operator :visited?]))
+    (doseq [operand (:operands visited)]
+      (get-in operand [:visited?]))))
+
+(deftest test-visit-defasm
+  (let [visited (visit (parse-defasm '(defasm foo (%add %rax 1))) visitor)]
+    (is (get-in visited [:visited?]))
+    (is (get-in visited [:name :visited?]))
+    (doseq [statement (:statements visited)]
+      (is (get-in statement [:visited?])))))
+
+(deftest test-visit-defextern
+  (let [visited (visit (parse-defextern '(defextern foo)) visitor)]
+    (is (get-in visited [:visited?]))
+    (is (get-in visited [:name :visited?]))))
