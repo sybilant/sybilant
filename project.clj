a0b70f1ead2b2f8efb5c14200984060daacfafae
Paul Stadig <paul@sybilant.org>
2014-04-22 06:34:11 -0400

    Generates initial project structure.

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject sybilant "0.0.1-SNAPSHOT"
+  :description "A \"full stack\" Lisp programming language for the x86-64
+  architecture."
+  :url "http://github.com/sybilant/sybilant/"
+  :license {:name "Mozilla Public License, v. 2.0"
+            :url "http://mozilla.org/MPL/2.0/"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :source-paths ["clojure/src"]
+  :test-paths ["clojure/test"]
+  :aot [sybilant.compile])
