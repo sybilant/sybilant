0cb68a549ca8e43d643299f5167693fcaa6662e5
Paul Stadig <paul@sybilant.org>
2014-05-31 06:21:24 -0400

    Add dir locals config for correct formatting of try+.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,9 +2,9 @@
   (fill-column . 80)
   (whitespace-line-column . nil))
  (clojure-mode
-  (clojure-test-ns-segment-position . 1)
   (eval ignore-errors
         (require 'whitespace)
         (whitespace-mode 0)
         (whitespace-mode 1)
-        (define-clojure-indent (%deftext 'defun)))))
+        (define-clojure-indent (%deftext 'defun))
+        (define-clojure-indent (try+ 'defun)))))
