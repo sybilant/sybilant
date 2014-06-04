af16ed4a8fb3d550a53c2c7119668f755a50337f
Paul Stadig <paul@sybilant.org>
2014-06-04 07:51:54 -0400

    Add test! alias for doing a clean before running tests.

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
   :test-paths ["clojure/test"]
   :aot [sybilant.compile]
   :main sybilant.compile
+  :aliases {"test!" ["do" "clean," "test"]}
   :profiles {:dev {:dependencies [[pjstadig/humane-test-output "0.6.0"]
                                   [robert/hooke "1.3.0"]]}
              :test {:injections [(require 'pjstadig.humane-test-output)
