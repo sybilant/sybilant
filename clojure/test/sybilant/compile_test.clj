55c24a94ce2c045fb491c4b5d1de6dae3638e349
Paul Stadig <paul@sybilant.org>
2014-09-19 19:54:45 -0400

    Don't bother with the copyright year.

--- clojure/test/sybilant/compile_test.clj
+++ clojure/test/sybilant/compile_test.clj
@@ -1,4 +1,4 @@
-;;;; Copyright © 2014 Paul Stadig.  All rights reserved.
+;;;; Copyright © Paul Stadig.  All rights reserved.
 ;;;;
 ;;;; This Source Code Form is subject to the terms of the Mozilla Public
 ;;;; License, v. 2.0.  If a copy of the MPL was not distributed with this file,
