9541df7e476b158d68a8d7ac537098d2dde58fb3
Paul Stadig <paul@sybilant.org>
2015-11-24 05:24:03 -0500

    A new adventure begins...

---
+++ clojure/test/sybilant/compile_test.clj
@@ -0,0 +1,16 @@
+;;;; Copyright © Paul Stadig.  All rights reserved.
+;;;;
+;;;; This Source Code Form is subject to the terms of the Mozilla Public
+;;;; License, v. 2.0.  If a copy of the MPL was not distributed with this file,
+;;;; You can obtain one at http://mozilla.org/MPL/2.0/.
+;;;;
+;;;; This Source Code Form is "Incompatible With Secondary Licenses", as defined
+;;;; by the Mozilla Public License, v. 2.0.
+(ns sybilant.compile-test
+  (:require
+   [clojure.test :refer [deftest is]]
+   [sybilant.compile :refer :all]))
+
+(deftest test-main
+  (is (= "Hello, World!\n" (with-out-str (-main))))
+  (is (= "Hello, World! foo\n" (with-out-str (-main "foo")))))
