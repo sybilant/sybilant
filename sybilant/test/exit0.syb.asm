e65056893c7573f725d23686bc973fdee80a1973
Paul Stadig <paul@sybilant.org>
2024-02-01 07:33:50 -0500

    Add first, minimal sybilant test

---
+++ sybilant/test/exit0.syb.asm
@@ -0,0 +1,14 @@
+;; Copyright © 2024 Paul Stadig
+;;
+;; This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.  If a copy
+;; of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; This Source Code Form is "Incompatible With Secondary Licenses", as defined by the Mozilla Public
+;; License, v. 2.0.
+section .text
+extern exit
+global _start
+
+_start:
+    mov rdi, 0
+    jmp exit
